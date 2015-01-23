ebf95a09011481e1b20883c464fe39dcf4c7f2ea
Yuriy Makhnovskyy <ymakhnovskyy@maven.co>
2015-01-23 14:30:27 +0200

    Initial commit.

---
+++ README.txt
@@ -0,0 +1,15 @@
+js.readmore
+***********
+
+Introduction
+============
+
+This library packages `CReadmore`_ for `fanstatic`_.
+
+.. _`fanstatic`: http://fanstatic.org
+.. _`readmore`: http://jedfoster.com/Readmore.js/
+
+This requires integration between your web framework and ``fanstatic``,
+and making sure that the original resources (shipped in the ``resources``
+directory in ``js.readmore``) are published to some URL.
+
---
+++ setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+import os
+
+# The version of the wrapped library is the starting point for the
+# version number of the python package.
+# In bugfix releases of the python package, add a '-' suffix and an
+# incrementing integer.
+# For example, a packaging bugfix release version 1.4.4 of the
+# js.jquery package would be version 1.4.4-1 .
+
+version = '2.0.0'
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+long_description = (
+    read('README.txt')
+    + '\n' +
+    read('js', 'readmore', 'test_chosen.txt')
+    + '\n' +
+    read('CHANGES.txt'))
+
+setup(
+    name='js.readmore',
+    version=version,
+    description="Fanstatic packaging of Readmore",
+    long_description=long_description,
+    classifiers=[],
+    keywords='',
+    author='Yuriy Mahnovskyy',
+    author_email='ymahnovskyy@gmail.com',
+    license='BSD',
+    packages=find_packages(),namespace_packages=['js'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'fanstatic',
+        'js.jquery',
+        'setuptools',
+        ],
+    entry_points={
+        'fanstatic.libraries': [
+            'readmore = js.readmore:library',
+            ],
+        },
+    )
