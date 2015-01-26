7b8e0c1d11175f8a239d3bdb2e4744c30c0b5c8e
Yuriy Makhnovskyy <ymakhnovskyy@maven.co>
2015-01-26 18:55:41 +0200

    Changed file structure.

---
+++ js/readmore/__init__.py
@@ -0,0 +1,13 @@
+from fanstatic import Library, Resource
+from js.jquery import jquery
+
+library = Library('js.readmore', 'resources')
+
+readmore_js = Resource(
+    library,
+    'readmore.js',
+    minified='readmore.min.js',
+    depends=[jquery]
+)
+
+readmore = readmore_js
